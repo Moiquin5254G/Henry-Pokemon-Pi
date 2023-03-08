048169117243cc9aa27cab61c8818e4cec6654c0
Moiquin5254G <112026738+Moiquin5254G@users.noreply.github.com>
2023-03-07 23:34:46 -0500

    Deploy

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ require('dotenv').config();
 const { PORT } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(async() => {
+conn.sync({ alter: false }).then(async() => {
   server.listen(PORT, () => {
     console.log('%s listening at', PORT); // eslint-disable-line no-console
   });
