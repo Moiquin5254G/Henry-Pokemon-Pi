850d296e9a27eec6275af9a77e03968fa9de26cf
Moiquin5254G <112026738+Moiquin5254G@users.noreply.github.com>
2023-03-07 23:53:10 -0500

    Deploy

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -28,7 +28,9 @@ export const HomePage = () => {
     const [pokemonsInPage, setPokemonsPerPage] = useState(12);
     const [currentPage, setCurrentPage] = useState(1);
     const lastPokemon = currentPage * pokemonsInPage;
+    console.log(lastPokemon);
     const firtsPokemon = lastPokemon - pokemonsInPage;
+    console.log(firtsPokemon);
     const renderPokemons = allPokemons.slice(firtsPokemon, lastPokemon);
     console.log(renderPokemons);
     const Page = (pageNumber) => { setCurrentPage(pageNumber) };
